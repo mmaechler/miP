9677766db2c22054fddeffdde3d612c16d45731a
Martin Maechler <maechler@r-project.org>
2015-11-09 09:35:04 +0100

    indent all R sources  (and save as UTF8 instead of 'DOS')

--- R/obs_plot.R
+++ R/obs_plot.R
@@ -1,19 +1,19 @@
 obs_plot <-
-function(ds,var,main="default")
+    function(ds,var,main="default")
 {
-var_nr = which(names(ds)==var)
-x = 1:length(names(ds))
-x = x[-var_nr]
-t = length(x)
-par(mfrow=c(ceiling(t/ceiling(sqrt(t))),ceiling(sqrt(t))))
-for (i in x)
-{
-eins = ds[,var_nr][!is.na(ds[,var_nr])][!is.na(ds[,i][!is.na(ds[,var_nr])])]
-obs = length(eins)/dim(ds)[1]
-if(main == "default")
-{main = paste(round(obs*100,2),"% observed")}
-plot(ds[,var_nr][!is.na(ds[,var_nr])],ds[,i][!is.na(ds[,var_nr])],main=main,font.main=2,pch=16,xlab=names(ds)[var_nr],ylab=names(ds)[i])
-lines(lowess(ds[,var_nr][!is.na(ds[,var_nr])][!is.na(ds[,i][!is.na(ds[,var_nr])])],ds[,i][!is.na(ds[,var_nr])][!is.na(ds[,i][!is.na(ds[,var_nr])])]),col="red")
-}
+    var_nr = which(names(ds)==var)
+    x = 1:length(names(ds))
+    x = x[-var_nr]
+    t = length(x)
+    par(mfrow=c(ceiling(t/ceiling(sqrt(t))),ceiling(sqrt(t))))
+    for (i in x)
+    {
+        eins = ds[,var_nr][!is.na(ds[,var_nr])][!is.na(ds[,i][!is.na(ds[,var_nr])])]
+        obs = length(eins)/dim(ds)[1]
+        if(main == "default")
+        {main = paste(round(obs*100,2),"% observed")}
+        plot(ds[,var_nr][!is.na(ds[,var_nr])],ds[,i][!is.na(ds[,var_nr])],main=main,font.main=2,pch=16,xlab=names(ds)[var_nr],ylab=names(ds)[i])
+        lines(lowess(ds[,var_nr][!is.na(ds[,var_nr])][!is.na(ds[,i][!is.na(ds[,var_nr])])],ds[,i][!is.na(ds[,var_nr])][!is.na(ds[,i][!is.na(ds[,var_nr])])]),col="red")
+    }
 }
 
